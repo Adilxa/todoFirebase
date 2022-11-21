936aa81dfd0f5ecbbf288687cbd3f8c739dd6350
Adilxa <adilhanimerov82gmail.com>
2022-11-21 05:58:25 +0300

    feat:new

--- src/todoitem/TodoItem.jsx
+++ src/todoitem/TodoItem.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import css from "./todoitem.module.scss";
 import { doc, updateDoc, deleteDoc } from "firebase/firestore";
 import { db } from '../firebase';
